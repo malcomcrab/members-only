fd533d00a15506ba2d2d20d1e98b3127acd01498
malcomcrab <malcomcrabesq@outlook.com>
2025-02-17 12:50:09 +0000

    Added CSS file for index.ejs. Begun adding formatting and adding style to index

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const bcrypt = require("bcryptjs")
 
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
